9439c49085c44aabe1dec98116dfc7cc306e2505
Wang Zunjiao <wangzunjiao.wzj@digital-engine.com>
2025-12-10 06:00:49 +0000

    false[antom-sdk-automation] automated change

--- ams-dotnet/src/com/alipay/ams/api/request/pay/AlipayVaultingPaymentMethodRequest.cs
+++ ams-dotnet/src/com/alipay/ams/api/request/pay/AlipayVaultingPaymentMethodRequest.cs
@@ -14,26 +14,28 @@ public class AlipayVaultingPaymentMethodRequest : AMSRequest<AlipayVaultingPayme
 
         
 
-        public AlipayVaultingPaymentMethodRequest( string vaultingRequestId , string vaultingNotificationUrl , string redirectUrl , string merchantRegion , PaymentMethodDetail paymentMethodDetail , Env env , string merchantAccountId , string vaultingCurrency , CustomizedInfo customizedInfo)
+        public AlipayVaultingPaymentMethodRequest( string merchantAccountId , string metadata , string vaultingRequestId , string vaultingNotificationUrl , string redirectUrl , string merchantRegion , PaymentMethodDetail paymentMethodDetail , Env env , string vaultingCurrency , CustomizedInfo customizedInfo)
         {
+            this.MerchantAccountId = merchantAccountId;
+            this.Metadata = metadata;
             this.VaultingRequestId = vaultingRequestId;
             this.VaultingNotificationUrl = vaultingNotificationUrl;
             this.RedirectUrl = redirectUrl;
             this.MerchantRegion = merchantRegion;
             this.PaymentMethodDetail = paymentMethodDetail;
             this.Env = env;
-            this.MerchantAccountId = merchantAccountId;
             this.VaultingCurrency = vaultingCurrency;
             this.CustomizedInfo = customizedInfo;
         }
 
+            public string MerchantAccountId { get; set; }
+            public string Metadata { get; set; }
             public string VaultingRequestId { get; set; }
             public string VaultingNotificationUrl { get; set; }
             public string RedirectUrl { get; set; }
             public string MerchantRegion { get; set; }
             public PaymentMethodDetail PaymentMethodDetail { get; set; }
             public Env Env { get; set; }
-            public string MerchantAccountId { get; set; }
             public string VaultingCurrency { get; set; }
             public CustomizedInfo CustomizedInfo { get; set; }
 
