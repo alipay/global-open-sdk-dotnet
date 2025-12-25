a74af456a591fb90e3c17284bec9dde0d3cd60ec
Wang Zunjiao <wangzunjiao.wzj@digital-engine.com>
2025-12-25 03:07:02 +0000

    false[antom-sdk-automation] automated change

---
+++ ams-dotnet/src/com/alipay/ams/api/response/aba/AlipayInquireCardResponse.cs
@@ -0,0 +1,34 @@
+using com.alipay.ams.api.entities;
+using System;
+using System.Collections.Generic;
+    
+namespace com.alipay.ams.api.response.aba
+{
+
+public class AlipayInquireCardResponse : AMSResponse
+    {
+
+        public AlipayInquireCardResponse() { }
+
+        
+
+        public AlipayInquireCardResponse( Result result , int? totalCount , int? totalPageNumber , int? currentPageNumber , List<AbaCard> cardList)
+        {
+            this.Result = result;
+            this.TotalCount = totalCount;
+            this.TotalPageNumber = totalPageNumber;
+            this.CurrentPageNumber = currentPageNumber;
+            this.CardList = cardList;
+        }
+
+            public Result Result { get; set; }
+            public int? TotalCount { get; set; }
+            public int? TotalPageNumber { get; set; }
+            public int? CurrentPageNumber { get; set; }
+            public List<AbaCard> CardList { get; set; }
+
+        
+
+    }
+
+}
