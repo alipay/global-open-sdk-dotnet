1324f52e942dd7cef4280b0b60d8cee050c4f271
Wang Zunjiao <wangzunjiao.wzj@digital-engine.com>
2026-01-19 10:04:59 +0000

    false[antom-sdk-automation] automated change

---
+++ ams-dotnet/src/com/alipay/ams/api/entities/RateType.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+    
+namespace com.alipay.ams.api.entities
+{
+
+public enum RateType
+    {
+            TRADE,
+            REFERENCE,
+            REFERENCE_TRADE,
+    }
+}
