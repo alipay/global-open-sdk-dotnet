afd031126e9143a2c9980a49fec7f5d7d192eb85
Wang Zunjiao <wangzunjiao.wzj@digital-engine.com>
2026-03-16 10:58:26 +0000

    false[antom-sdk-automation] automated change

---
+++ ams-dotnet/src/com/alipay/ams/api/request/aba/AlipayInquireDirectRefundRequest.cs
@@ -0,0 +1,31 @@
+using com.alipay.ams.api.response.aba;
+using com.alipay.ams.api.request;
+using com.alipay.ams.api.entities;
+using System;
+using System.Collections.Generic;
+    
+namespace com.alipay.ams.api.request.aba
+{
+
+public class AlipayInquireDirectRefundRequest : AMSRequest<AlipayInquireDirectRefundResponse>
+    {
+
+        public AlipayInquireDirectRefundRequest() { }
+
+        
+
+        public AlipayInquireDirectRefundRequest( string refundId , string refundRequestId)
+        {
+            this.RefundId = refundId;
+            this.RefundRequestId = refundRequestId;
+        }
+
+            public string RefundId { get; set; }
+            public string RefundRequestId { get; set; }
+
+        public override string GetRequestURI(){ return "/ams/aba/funds/inquireDirectRefund"; } 
+
+
+    }
+
+}
