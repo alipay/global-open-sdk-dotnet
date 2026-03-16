73831653acb5ce9b997d973c65432702dc499ed9
Wang Zunjiao <wangzunjiao.wzj@digital-engine.com>
2026-03-16 09:46:52 +0000

    false[antom-sdk-automation] automated change

---
+++ ams-dotnet/src/com/alipay/ams/api/response/aba/InquireDirectPaymentResponse.cs
@@ -0,0 +1,44 @@
+using com.alipay.ams.api.entities;
+using System;
+using System.Collections.Generic;
+    
+namespace com.alipay.ams.api.response.aba
+{
+
+public class InquireDirectPaymentResponse : AMSResponse
+    {
+
+        public InquireDirectPaymentResponse() { }
+
+        
+
+        public InquireDirectPaymentResponse( TransactionStatusType paymentStatus , string paymentResultMessage , string paymentResultCode , string paymentId , string paymentRequestId , PaymentMethod payToMethod , Amount paymentAmount , Amount payToAmount , string paymentTime , Result result)
+        {
+            this.PaymentStatus = paymentStatus;
+            this.PaymentResultMessage = paymentResultMessage;
+            this.PaymentResultCode = paymentResultCode;
+            this.PaymentId = paymentId;
+            this.PaymentRequestId = paymentRequestId;
+            this.PayToMethod = payToMethod;
+            this.PaymentAmount = paymentAmount;
+            this.PayToAmount = payToAmount;
+            this.PaymentTime = paymentTime;
+            this.Result = result;
+        }
+
+            public TransactionStatusType PaymentStatus { get; set; }
+            public string PaymentResultMessage { get; set; }
+            public string PaymentResultCode { get; set; }
+            public string PaymentId { get; set; }
+            public string PaymentRequestId { get; set; }
+            public PaymentMethod PayToMethod { get; set; }
+            public Amount PaymentAmount { get; set; }
+            public Amount PayToAmount { get; set; }
+            public string PaymentTime { get; set; }
+            public Result Result { get; set; }
+
+        
+
+    }
+
+}
---
+++ ams-dotnet/src/com/alipay/ams/api/response/aba/InquireDirectRefundResponse.cs
@@ -0,0 +1,46 @@
+using com.alipay.ams.api.entities;
+using System;
+using System.Collections.Generic;
+    
+namespace com.alipay.ams.api.response.aba
+{
+
+public class InquireDirectRefundResponse : AMSResponse
+    {
+
+        public InquireDirectRefundResponse() { }
+
+        
+
+        public InquireDirectRefundResponse( TransactionStatusType refundStatus , string refundResultMessage , string refundResultCode , string refundId , string paymentId , string refundRequestId , string refundTime , RefundFromMethod refundFromMethod , Amount refundToAmount , Amount refundFromAmount , Result result)
+        {
+            this.RefundStatus = refundStatus;
+            this.RefundResultMessage = refundResultMessage;
+            this.RefundResultCode = refundResultCode;
+            this.RefundId = refundId;
+            this.PaymentId = paymentId;
+            this.RefundRequestId = refundRequestId;
+            this.RefundTime = refundTime;
+            this.RefundFromMethod = refundFromMethod;
+            this.RefundToAmount = refundToAmount;
+            this.RefundFromAmount = refundFromAmount;
+            this.Result = result;
+        }
+
+            public TransactionStatusType RefundStatus { get; set; }
+            public string RefundResultMessage { get; set; }
+            public string RefundResultCode { get; set; }
+            public string RefundId { get; set; }
+            public string PaymentId { get; set; }
+            public string RefundRequestId { get; set; }
+            public string RefundTime { get; set; }
+            public RefundFromMethod RefundFromMethod { get; set; }
+            public Amount RefundToAmount { get; set; }
+            public Amount RefundFromAmount { get; set; }
+            public Result Result { get; set; }
+
+        
+
+    }
+
+}
